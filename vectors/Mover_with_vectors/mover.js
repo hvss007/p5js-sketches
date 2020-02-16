7227525c1e28912f06e433c50fe4d35316ccd0c1
Harsh Vardhan <hvss007@gmail.com>
2020-02-16 16:36:27 +0530

    Mouse seeking acceleration

--- vectors/Mover_with_vectors/mover.js
+++ vectors/Mover_with_vectors/mover.js
@@ -1,11 +1,12 @@
 class Mover{
+    t=0;
     location;
     velocity;
     acceleration;
     constructor(){
         this.location=createVector(width/2,height/2)
         this.velocity=createVector(1,3)
-        this.acceleration=createVector(0.02,-0.03)
+        // this.acceleration=createVector(0.02,-0.03)
     }
   display(){
       fill(42,54,62)
@@ -13,8 +14,14 @@ class Mover{
       ellipse(this.location.x,this.location.y,90,90)
   }  
    update(){
+       var mouse=createVector(mouseX,mouseY)
+       //var direction=mouse.sub(location)
+       this.acceleration=p5.Vector.sub(mouse,this.location)
+       //this.acceleration=p5.Vector.random2D().mult(noise(this.t))
        this.location.add(this.velocity)
        this.velocity.add(this.acceleration)
+       this.velocity.limit(5)
+       this.t++
    } 
    checkEdges(){
        if(this.location.x>width){
